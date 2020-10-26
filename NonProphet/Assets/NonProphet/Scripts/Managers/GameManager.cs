dc18d1cdea15d8304b7d09f2d38469cdf1f8e21a
Philip-Waldron <philip_waldron@yahoo.com>
2020-10-26 16:13:50 +0000

    Move managers to managers folder

---
+++ NonProphet/Assets/NonProphet/Scripts/Managers/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public bool UseVR = true;
+    public Camera DefaultCamera;
+    public Camera VRCamera;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+        Debug.Log("GameManager UseVR = " + UseVR);
+        XRSettings.enabled = UseVR;
+        DefaultCamera.gameObject.SetActive(!IsUsingVR());
+        VRCamera.gameObject.SetActive(IsUsingVR());
+    }
+
+    public bool IsUsingVR()
+    {
+        return UseVR && IsXRDevicePresent();
+    }
+
+    public static bool IsXRDevicePresent()
+    {
+        var xrDisplaySubsystems = new List<XRDisplaySubsystem>();
+        SubsystemManager.GetInstances(xrDisplaySubsystems);
+        return xrDisplaySubsystems.Any(xrDisplay => xrDisplay.running);
+    }
+
+    private void OnEnable()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+}
